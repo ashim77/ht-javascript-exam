e23976a60d239b43b41155119221bb2428c7fef1
Ashim Kumar <simt.ashim@gmail.com>
2023-09-10 14:45:53 +0600

    javascript  exam

--- exam-two/qsix.js
+++ exam-two/qsix.js
@@ -8,25 +8,6 @@ const people = [
     { name: "Bob", age: 35 },
 ];
 
-const data = ascendingOrder(people, "age");
+const data = ascendingOrder(people, "name");
 
 console.log(data);
-
-// const a = ['Sweety', 'Ashim', 'Kumar', 'Sirja'];
-
-console.log(a.sort());
-
-// function sortByKey(arr, key) {
-//     return arr.sort((a, b) => a[key] - b[key]);
-//     // console.log(arr);
-//     // console.log(arr.slice());
-// }
-
-// const people = [
-//     { name: "John", age: 30 },
-//     { name: "Alice", age: 25 },
-//     { name: "Bob", age: 35 },
-// ];
-
-// const sortedPeople = sortByKey(people, "age");
-// console.log(sortedPeople);
