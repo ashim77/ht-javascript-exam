98661e9f20e68a85e3ce5ddb7d21a2f5ac0e6983
Ashim Kumar <simt.ashim@gmail.com>
2024-12-03 21:32:18 +0600

    number

---
+++ number.js
@@ -0,0 +1,19 @@
+let a = (0.5 * 10 + 0.1 * 10) / 10;
+let x = 0xFF;
+let myNumber = 8;
+// console.log(myNumber.toString(10));
+// console.log(myNumber.toString(32));
+// console.log(myNumber.toString(16));
+// console.log(myNumber.toString(8));
+// console.log(myNumber.toString(2));
+// console.log(x);
+
+// 32/2 = 16 - 0
+// 16/2 = 8 - 0
+// 8/2 = 4 - 0
+// 4/2 = 2 - 0
+// 2/2 = 0 
+
+// console.log((20 + 90).toString());
+// console.log(Number(new Date("2024-12-12")));
+console.log(parseInt("10.86"));
